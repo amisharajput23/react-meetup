7ddc1ee3ee651d49711cfcd4919547a00a570859
amisha <rajputamisha34@gmail.com>
2021-06-03 20:23:06 +0530

    fetching api via firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,29 @@
 import NewMeetupForm from "../Components/meetups/NewMeetupForm";
+import { useHistory } from 'react-router-dom';
 
 function NewMeetUpPage(){
+const history = useHistory();
+
+function addMeetupHandler(meetupData){
+   fetch('https://react-meetup-25c88-default-rtdb.firebaseio.com/meetups.json',
+   {
+       method: 'POST',
+       body: JSON.stringify(meetupData),
+       headers: {
+           'Content-Type': 'application/json'
+       }
+   }
+
+   ).then(() => {
+       history.replace('/');
+   })
+}
+
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm  onAddMeetup={addMeetupHandler}/>
         </section>
     )
 }
